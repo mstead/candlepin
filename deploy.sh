46b66b4993efe850fc87058f5f126abef00b1c4f
Filip Nguyen <fnguyen@redhat.com>
2016-03-10 14:01:04 +0100

    Adding script to deploy with SQL dump

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DUMPDIR=/home/fnguyen/candlepin/perf-issue/localdumps
+ADIR=/home/fnguyen/candlepin/perf-issue/adapters/20
+
+buildr clean && server/bin/deploy -gm
+cd $DUMPDIR
+sudo systemctl stop tomcat
+sudo rm -rf /var/log/candlepin/*
+mysql -u candlepin candlepin < 20.sql
+cd $ADIR
+sleep 3
+./install.sh
+echo "Calling status resource"
+sleep 10
+curl -X GET http://localhost:8080/candlepin/status
